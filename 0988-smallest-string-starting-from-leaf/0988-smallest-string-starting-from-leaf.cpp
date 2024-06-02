e093cc8ed6a19c53e18b4e144a7f9c5e795e1bc7
Arpit Rathore <arpitrathore009@gmail.com>
2024-06-02 14:05:50 +0530

    Time: 7 ms (77.42%), Space: 22.1 MB (5.20%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -11,45 +11,39 @@
  */
 class Solution {
 public:
-    
-    void dfsTraversal(TreeNode* root,string currString,string &smallestString){
-        if(root==NULL){
-            return;
-        }
-        currString+=(char)(97+root->val);
-        if(root->left==NULL && root->right==NULL){
-            if(smallestString == ""){
-                smallestString = currString;
-            }
-            else{
-                int sptr1=currString.length()-1,sptr2=smallestString.length()-1;
-                bool isSmallAlready=false;
-                while(sptr1>=0 && sptr2>=0){
-                    if(smallestString[sptr2] < currString[sptr1]){
-                        isSmallAlready=true;
-                        break;
-                    }
-                    if(currString[sptr1] < smallestString[sptr2]){
-                        smallestString=currString;
-                        break;
-                    }
-                    --sptr1;
-                    --sptr2;
-                }
-                if(!isSmallAlready){
-                    smallestString=(sptr1 < 0 && sptr2 >=0)?currString:smallestString;   
-                }
-            }
-            return;
-        }
-        dfsTraversal(root->left,currString,smallestString);
-        dfsTraversal(root->right,currString,smallestString);
-    }
-    
     string smallestFromLeaf(TreeNode* root) {
-        string smallestString="",currString="";
-        dfsTraversal(root,currString,smallestString);
-        reverse(smallestString.begin(),smallestString.end());
+        string smallestString="";
+        queue<pair<TreeNode*,string>>qu;
+        pair<TreeNode*,string>p;
+        p.first=root;
+        p.second=(char)(97+root->val);
+        qu.push(p);
+        while(!qu.empty()){
+            int size=qu.size();
+            pair<TreeNode*,string>tmp;
+            for(int i=0;i<size;i++){
+                pair<TreeNode*,string>frontNodePair=qu.front();
+                qu.pop();
+                if(frontNodePair.first->left!=NULL){
+                    tmp.first=frontNodePair.first->left;
+                    tmp.second=(char)(97+frontNodePair.first->left->val)+frontNodePair.second;
+                    qu.push(tmp);
+                }
+                if(frontNodePair.first->right!=NULL){
+                    tmp.first=frontNodePair.first->right;
+                    tmp.second=(char)(97+frontNodePair.first->right->val)+frontNodePair.second;
+                    qu.push(tmp);
+                }
+                if(frontNodePair.first->left==NULL && frontNodePair.first->right==NULL){
+                    if(smallestString==""){
+                        smallestString = frontNodePair.second;
+                    }
+                    else{
+                        smallestString=min(smallestString,frontNodePair.second);
+                    }
+                }
+            }
+        }
         return smallestString;
     }
 };
