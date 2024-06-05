ff78d30fdc05f406d843e5460fa570ca21913779
Arpit Rathore <arpitrathore009@gmail.com>
2024-06-05 12:51:45 +0530

    Time: 6 ms (93.2%), Space: 21.7 MB (5.71%) - LeetHub

--- 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/1450-delete-leaves-with-a-given-value.cpp
@@ -11,30 +11,13 @@
  */
 class Solution {
 public:
-
-    void dfsTraversal(TreeNode* root,int &target,TreeNode* parent){
-        if(root==NULL){
-            return;
-        }
-        dfsTraversal(root->left,target,root);
-        dfsTraversal(root->right,target,root);
-        if(root->left==NULL && root->right==NULL && root->val == target){
-            if(parent!=NULL){
-                if(parent->left==root){
-                    delete(parent->left);
-                    parent->left=NULL;
-                }
-                if(parent->right==root){
-                    delete(parent->right);
-                    parent->right=NULL;
-                }
-            }
-        }
-    }
-
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        dfsTraversal(root,target,NULL);
-        if(root->left==NULL && root->right==NULL && root->val==target){  
+        if(root==NULL){
+            return NULL;
+        }
+        root->left=removeLeafNodes(root->left,target);
+        root->right=removeLeafNodes(root->right,target);
+        if(root->left==NULL && root->right==NULL && root->val==target){
             return NULL;
         }
         return root;
