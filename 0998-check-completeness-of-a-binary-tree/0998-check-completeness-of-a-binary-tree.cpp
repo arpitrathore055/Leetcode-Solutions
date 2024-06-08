9812ce2943ee65577726a4405bc8aebab0caba97
Arpit Rathore <arpitrathore009@gmail.com>
2024-06-08 12:16:45 +0530

    Time: 4 ms (45.63%), Space: 12.6 MB (97.68%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -10,27 +10,31 @@
  * };
  */
 class Solution {
+    bool completeTree=true;
 public:
-    bool isCompleteTree(TreeNode* root) {
-        queue<TreeNode*>qu;
-        bool containsNullBeforeNode=false;
-        qu.push(root);
-        while(!qu.empty()){
-            int size=qu.size();
-            for(int i=0;i<size;i++){
-                TreeNode* frontNode=qu.front();
-                qu.pop();
-                if(frontNode==NULL){
-                    containsNullBeforeNode=true;
-                    continue;
-                }
-                if(containsNullBeforeNode){
-                    return false;
-                }
-                qu.push(frontNode->left);
-                qu.push(frontNode->right);
-            }
+
+    int countNodes(TreeNode* root){
+        if(root==NULL){
+            return 0;
         }
-        return true;
+        return countNodes(root->left) + 1 + countNodes(root->right);
+    }
+
+    void dfsTraversal(TreeNode* root,int &countOfNodes,int currInd){
+        if(root==NULL){
+            return;
+        }
+        if(currInd>countOfNodes){
+            completeTree=false;
+            return;
+        }
+        dfsTraversal(root->left,countOfNodes,(2*currInd));
+        dfsTraversal(root->right,countOfNodes,(2*currInd)+1);
+    }
+
+    bool isCompleteTree(TreeNode* root) {
+        int countOfNodes=countNodes(root);
+        dfsTraversal(root,countOfNodes,1);
+        return completeTree;
     }
 };
