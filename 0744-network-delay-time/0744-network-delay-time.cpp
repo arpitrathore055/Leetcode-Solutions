5ae7b181d8652b1fbe88d750159dbbd7507ce9a0
Arpit Rathore <arpitrathore009@gmail.com>
2024-06-29 12:03:32 +0530

    Time: 91 ms (37.8%), Space: 40.1 MB (98.89%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int,vector<pair<int,int>>>umap;
-        for(auto& time:times){
-            int u=time[0];
-            int v=time[1];
-            int reqtime=time[2];
-            umap[u].push_back({v,reqtime});
-        }
         vector<int>reqTime(n+1,INT_MAX);
-        set<pair<int,int>>st;
         reqTime[k]=0;
-        st.insert({0,k});
-        while(!st.empty()){
-            auto& it=*st.begin();
-            int vertex=it.second;
-            int signalReachTime=it.first;
-            st.erase(it);
-            for(auto& v:umap[vertex]){
-                int nextVertex=v.first;
-                int nextVertexReachTime=v.second;
-                
-                if(signalReachTime+nextVertexReachTime < reqTime[nextVertex]){
-
-                    if(reqTime[nextVertex]!=INT_MAX){
-                        st.erase({reqTime[nextVertex],nextVertex});
-                    }
-
-                    reqTime[nextVertex]=signalReachTime+nextVertexReachTime;
-                    st.insert({reqTime[nextVertex],nextVertex});
+        for(int i=0;i<=n-1;i++){
+            for(auto& time:times){
+                int u=time[0];
+                int v=time[1];
+                int weight=time[2];
+                if(reqTime[u]!=INT_MAX && reqTime[u]+weight < reqTime[v]){
+                    reqTime[v]=reqTime[u]+weight;
                 }
-
             }
         }
-        int minTimeReq=0;
-        for(int i=1;i<n+1;i++){
-            
+        int maxReqTime=0;
+        for(int i=1;i<=n;i++){
             if(reqTime[i]==INT_MAX){
                 return -1;
-            }
-            else{
-                if(reqTime[i] > minTimeReq){
-                    minTimeReq=reqTime[i];
-                }
-            }
 
+            }
+            if(reqTime[i] > maxReqTime){
+                maxReqTime=reqTime[i];
+            }
         }
-        return minTimeReq;
+        return maxReqTime;
     }
 };
