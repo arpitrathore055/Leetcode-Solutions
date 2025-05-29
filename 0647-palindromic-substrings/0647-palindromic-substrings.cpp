596eeb8d386067e5cb0482aac3862372eb59b3d4
Arpit Rathore <arpitrathore009@gmail.com>
2025-05-29 17:48:55 +0530

    Time: 1907 ms (5.01%), Space: 418.9 MB (7.89%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    int dp[1001][1001];
+
+    int check(int i,int j,string s){
+        if(i > j){
+            return true;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i] == s[j]){
+            return dp[i][j]=check(i+1,j-1,s);
+        }
+        return false;
+    }
+
+    int countSubstrings(string s) {
+        int len=s.length(),count=0;
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<len;i++){
+            for(int j=i;j<len;j++){
+                if(check(i,j,s)){
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
