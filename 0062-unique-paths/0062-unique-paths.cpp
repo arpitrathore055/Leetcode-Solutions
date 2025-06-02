0ffd3bcd70a64fcace28acfdbbf42cdafb90159b
Arpit Rathore <arpitrathore009@gmail.com>
2025-06-02 12:26:59 +0530

    Time: 0 ms (100%), Space: 8 MB (85.98%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int dp[101][101];
+
+    int solve(int i,int j,int& m,int& n){
+        //base case
+        if(i >= m || j >= n){
+            return 0;
+        }
+        if(i == (m-1) && j == (n-1)){
+            return 1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int rightWays=solve(i,j+1,m,n);
+        int downWays=solve(i+1,j,m,n);
+        return dp[i][j]=rightWays+downWays;
+
+    }
+
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m,n);
+    }
+};
