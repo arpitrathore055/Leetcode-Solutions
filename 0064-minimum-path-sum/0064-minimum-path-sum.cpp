667af51b5d19ad23f646197cc0a5b888bcec3c74
Arpit Rathore <arpitrathore009@gmail.com>
2025-06-03 16:52:41 +0530

    Time: 0 ms (100%), Space: 15.3 MB (94.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int m,n,dp[201][201];
-
-    int solve(int i,int j,vector<vector<int>>& grid){
-        //base case
-        if(i >= m || j >= n){
-            return INT_MAX;
-        }
-        if(i == (m-1) && j == (n-1)){
-            return grid[m-1][n-1];
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        return dp[i][j]=grid[i][j]+min(solve(i+1,j,grid),solve(i,j+1,grid));
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
-        m=grid.size(),n=grid[0].size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,grid);
+        int m=grid.size(),n=grid[0].size();
+        for(int i=1;i<m;i++){
+            grid[i][0]+=grid[i-1][0];
+        }
+        for(int j=1;j<n;j++){
+            grid[0][j]+=grid[0][j-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
