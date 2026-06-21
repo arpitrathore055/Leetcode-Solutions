b61094afeb9f79c5109a5498de1175c4d142954c
Arpit Rathore <arpitrathore009@gmail.com>
2026-06-21 12:52:53 +0530

    Time: 257 ms (52.23%), Space: 10.8 MB (51.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,58 @@
 class Solution {
 public:
-
-    bool dfs(vector<vector<char>>& board,int i,int j,int& row,int& col,string& word,int ind){
-
-        //base case
-        if(ind == word.size()){
+    
+    vector<pair<int,int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
+    
+    bool backtrack(int i,int j,int wordInd,vector<vector<char>>& board,string& word,vector<vector<bool>>& visited,int& rows,int& cols){
+        
+        //basecase
+        if(wordInd == word.size()){
             return true;
         }
-
-        //pruning case
-        if(i < 0 || i >= row || j < 0 || j >=col || board[i][j] != word[ind]){
-            return false;
+        visited[i][j]=true;
+        
+        //choices
+        for(auto [xval,yval]:directions){
+            
+            //prunning
+            int newx=i + xval;
+            int newy=j + yval;
+            if(newx < 0 || newx >= rows || newy < 0 || newy >= cols || visited[newx][newy] || board[newx][newy] != word[wordInd]){
+                continue;
+            }
+            
+            //apply and undo
+            bool ans=backtrack(newx,newy,wordInd+1,board,word,visited,rows,cols);
+            if(ans){
+                return true;
+            }
         }
-
-        //apply
-        char temp=board[i][j];
-        board[i][j]='#';
-
-        //recursion
-        bool found=dfs(board,i+1,j,row,col,word,ind+1) || dfs(board,i,j+1,row,col,word,ind+1) || dfs(board,i-1,j,row,col,word,ind+1) || dfs(board,i,j-1,row,col,word,ind+1);
-
-        //undo
-        board[i][j]=temp;
-        return found;
-
+        
+        visited[i][j]=false;
+        return false;
+        
     }
-
-    bool exist(vector<vector<char>>& board, string word) {
     
-        int row=board.size();
-        int col=board[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(board[i][j] == word[0]){
-                    if(dfs(board,i,j,row,col,word,0)){
-                        return true;
-                    }
+    bool exist(vector<vector<char>>& board, string word) {
+       
+       bool wordPresent=false;
+       int rows=board.size();
+       int cols=board[0].size();
+       vector<vector<bool>> visited(rows,vector<bool>(cols,false));
+       
+       int wordInd=0;
+       
+       for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(!visited[i][j] && board[i][j] == word[wordInd]){
+                wordPresent=backtrack(i,j,wordInd+1,board,word,visited,rows,cols);
+                if(wordPresent){
+                    return true;
                 }
             }
         }
-        return false;
+       }
+       
+       return wordPresent;
     }
 };
