cbbc2a7918d30a2cc5f93191a45a9e2e92690820
Arpit Rathore <arpitrathore009@gmail.com>
2026-06-21 18:47:23 +0530

    Time: 439 ms (19.91%), Space: 8.9 MB (37.58%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,50 +1,78 @@
 class Solution {
 public:
-
-    bool canPlaceCh(vector<vector<char>>& board,int i,int j,char ch){
-
-        for(int k=0;k<9;k++){
-            if(board[i][k] == ch){
-                return false;
-            }
-            if(board[k][j] == ch){
-                return false;
-            }
-            int br = 3 * (i / 3) + k / 3;
-            int bc = 3 * (j / 3) + k % 3;
-            if(board[br][bc] == ch){
-                return false;
+    
+    bool isSafePlacement(char choice,int x,int y,vector<vector<char>>& board){
+        
+        int rows=board.size();
+        int cols=board[0].size();
+        
+        //check vertically
+        for(int i=0;i<rows;i++){
+           if(board[i][y] == choice){
+            return false;
+           } 
+        }
+        
+        //check horizontally
+        for(int j=0;j<cols;j++){
+           if(board[x][j] == choice){
+            return false;
+           } 
+        }
+        
+        //check grid
+        for(int i=((x/3)*3);i<(((x/3)*3)+3);i++){
+            for(int j=((y/3)*3);j<(((y/3)*3)+3);j++){
+    
+                if(board[i][j] == choice){
+                    return false;
+                }
+    
             }
         }
         return true;
-
     }
-
+    
     bool backtrack(vector<vector<char>>& board){
-
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        
+        for(int i=0;i<board.size();i++){
+            
+            for(int j=0;j<board[0].size();j++){
+                
                 if(board[i][j] == '.'){
-                    for(char ch='1';ch<='9';ch++){
-                        if(!canPlaceCh(board,i,j,ch)){
+                    
+                    //choices
+                    for(char choice='1';choice<='9';choice++){
+                        
+                        //prunning
+                        if(!isSafePlacement(choice,i,j,board)){
                             continue;
                         }
-                        board[i][j]=ch;
-                        if(backtrack(board)){
+                        
+                        board[i][j]=choice;
+                        
+                        bool gotAns=backtrack(board);
+                        if(gotAns){
                             return true;
                         }
+                        
                         board[i][j]='.';
+                        
                     }
                     return false;
                 }
+                
             }
+            
         }
+        
         return true;
-
-
+        
     }
-
+    
     void solveSudoku(vector<vector<char>>& board) {
+  
         backtrack(board);
+        
     }
 };
