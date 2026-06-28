d63470d79ca7fc26bc8a639650d8062a7e841977
Arpit Rathore <arpitrathore009@gmail.com>
2026-06-28 20:27:00 +0530

    Time: 5 ms (44.54%), Space: 16.4 MB (7.48%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,50 +9,51 @@
  */
 class Solution {
 public:
-    
-    void dfsTraversal(TreeNode* root,TreeNode* parent,TreeNode* target,unordered_map<int,pair<bool,pair<TreeNode*,int>>> &umap){
-        if(root==NULL){
+
+    unordered_map<TreeNode*,TreeNode*> parentMapping;
+    unordered_set<TreeNode*> visitedNodes;
+
+    void dfsForParentMapping(TreeNode* root,TreeNode* parent){
+
+        if(root == nullptr){
             return;
         }
-        pair<bool,pair<TreeNode*,int>>nodeDetails(false,pair<TreeNode*,int>(parent,(root == target)?0:INT_MAX));
-        umap[root->val]=nodeDetails;
-        dfsTraversal(root->left,root,target,umap);
-        dfsTraversal(root->right,root,target,umap);
-    }
-    
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        //<visited,parent,dist>
-        vector<int>nodesAtDistK;
-        unordered_map<int,pair<bool,pair<TreeNode*,int>>> umap;
-        dfsTraversal(root,NULL,target,umap);
-        queue<TreeNode*>qu;
-        qu.push(target);
-        umap[target->val].first=true;
-        while(!qu.empty()){
-            int size=qu.size();
-            for(int i=0;i<size;i++){
-                TreeNode* frontNode=qu.front();
-                qu.pop();
-                if(umap[frontNode->val].second.second == k){
-                    nodesAtDistK.push_back(frontNode->val);
-                }
-                if(frontNode->left!=NULL && umap[frontNode->left->val].first==false){
-                    qu.push(frontNode->left);
-                    umap[frontNode->left->val].first=true;
-                    umap[frontNode->left->val].second.second=umap[frontNode->val].second.second+1;
-                }
-                if(frontNode->right!=NULL && umap[frontNode->right->val].first==false){
-                    qu.push(frontNode->right);
-                    umap[frontNode->right->val].first=true;
-                    umap[frontNode->right->val].second.second=umap[frontNode->val].second.second+1;
-                }
-                if(umap[frontNode->val].second.first!=NULL && umap[umap[frontNode->val].second.first->val].first==false){
-                    qu.push(umap[frontNode->val].second.first);
-                    umap[umap[frontNode->val].second.first->val].first=true;
-                    umap[umap[frontNode->val].second.first->val].second.second=umap[frontNode->val].second.second+1;    
-                }
-            }
+        if(parent != nullptr){
+            parentMapping[root]=parent;
         }
-        return nodesAtDistK;
+        dfsForParentMapping(root->left,root);
+        dfsForParentMapping(root->right,root);
+
+    }
+
+    void dfsTraversal(TreeNode* source,int& k,int currDist,vector<int>& nodesAtKDist){
+
+        visitedNodes.insert(source);
+
+        if(currDist == k){
+            nodesAtKDist.push_back(source->val);
+            return;
+        }
+
+        if(source->left != nullptr && !visitedNodes.count(source->left)){
+            dfsTraversal(source->left,k,currDist+1,nodesAtKDist);
+        }
+        if(source->right != nullptr && !visitedNodes.count(source->right)){
+            dfsTraversal(source->right,k,currDist+1,nodesAtKDist);
+        }
+        if(parentMapping.find(source) != parentMapping.end() && !visitedNodes.count(parentMapping[source])){
+            dfsTraversal(parentMapping[source],k,currDist+1,nodesAtKDist);
+        }
+
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        vector<int> nodesAtKDist;
+        
+        dfsForParentMapping(root,nullptr);
+        dfsTraversal(target,k,0,nodesAtKDist);
+
+        return nodesAtKDist;
     }
 };
