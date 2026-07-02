a6db4d908e872bb307ff9780d739969b83bec676
Arpit Rathore <arpitrathore009@gmail.com>
2026-07-02 22:59:23 +0530

    Time: 0 ms (100%), Space: 20.9 MB (50.96%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-
-    void setAdjacentRowsAndCols(int i,int j,int rows,int cols,vector<vector<int>>& matrix){
-
-        //setting vertically cells
-        for(int r=0;r<rows;r++){
-            matrix[r][j]=0;
-        }
-
-        //setting horizontally cells
-        for(int c=0;c<cols;c++){
-            matrix[i][c]=0;
-        }
-
-    }
-
     void setZeroes(vector<vector<int>>& matrix) {
         
         int rows=matrix.size();
         int cols=matrix[0].size();
 
-        vector<pair<int,int>> coordinates;
+        vector<int> rowsTrace(rows,-1);
+        vector<int> colsTrace(cols,-1);
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(matrix[i][j] == 0){
-                    coordinates.push_back({i,j});
+                    rowsTrace[i]=0;
+                    colsTrace[j]=0;
                 }
             }
         }
-        for(auto [i,j]:coordinates){
-            setAdjacentRowsAndCols(i,j,rows,cols,matrix);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(rowsTrace[i] == 0 || colsTrace[j] == 0){
+                    matrix[i][j]=0;
+                }
+            }
         }
+
     }
 };
