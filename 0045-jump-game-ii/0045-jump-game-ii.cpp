779c1e1ef405812f8915ea7f10bab87a8b2990f2
Arpit Rathore <arpitrathore009@gmail.com>
2026-07-14 00:37:57 +0530

    Time: 0 ms (100%), Space: 20.5 MB (84.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,45 +1,23 @@
-#include<bits/stdc++.h>
-
 class Solution {
 public:
-
-    int dp[10001];
-
-    int solve(int index,int n,vector<int>& nums){
-
-        //prunning
-        if(index >= n){
-            return INT_MAX;
-        }
-
-        //basecase
-        if(index == n-1){
-            return 0;
-        }
-
-        //caching
-        if(dp[index] != -1){
-            return dp[index];
-        }
-
-        //ans compute
-        int minJumps=INT_MAX;
-        for(int i=1;i<=nums[index];i++){
-            int ans=solve(index + i,n,nums);
-            if(ans != INT_MAX){
-                minJumps=min(minJumps,ans + 1);
-            }
-        }
-
-        //save and return
-        return dp[index]=minJumps;
-    
-    }
-
     int jump(vector<int>& nums) {
         
-        memset(dp,-1,sizeof(dp));
-        return solve(0,nums.size(),nums);
+        int minJumps=0;
+        int insideBoundary=0;
+        int farthestBoundary=0;
+
+        for(int i=0;i<nums.size()-1;i++){
+
+            farthestBoundary=max(farthestBoundary,i + nums[i]);
+
+            if(i == insideBoundary){
+                minJumps++;
+                insideBoundary=farthestBoundary;
+            }
+
+        }
+
+        return minJumps;
 
     }
 };
