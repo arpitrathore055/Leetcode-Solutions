18c41e6d88f483b0fbb775223e6a616afa34a22c
Arpit Rathore <arpitrathore009@gmail.com>
2026-07-19 09:58:30 +0530

    Time: 39 ms (94.95%), Space: 29 MB (92.63%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,44 @@
 class Solution {
-public:
+   public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>finalTriplets;
-        set<vector<int>> unst;
-        int n=nums.size();
-        if(n < 3){
-            return finalTriplets; 
-        }
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> tripletsCollection;
+        int n = nums.size();
 
-        for(int i=0;i<nums.size()-2;i++){
+        sort(begin(nums), end(nums));
 
-            int left=i+1,right=nums.size()-1;
-            while(left<right){
-                if((nums[i] + nums[left] + nums[right]) == 0){
-                    if(unst.find({nums[i],nums[left],nums[right]}) == unst.end()){
-                        finalTriplets.push_back({nums[i],nums[left],nums[right]});
-                        unst.insert({nums[i],nums[left],nums[right]});
-                    }
-                    left++;right--;
-                }
-                else if((nums[i] + nums[left] + nums[right]) > 0){
-                    right--;
-                }
-                else{
-                    left++;
-                }
+        for (int i = 0; i < n - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int target = -nums[i];
+            int start = i + 1;
+            int end = n - 1;
+
+            if ((nums[start] + nums[start + 1]) > target || (nums[end] + nums[end - 1]) < target) {
+                continue;
             }
 
+            while (start < end) {
+                int value = nums[start] + nums[end];
+                if (value == target) {
+                    tripletsCollection.push_back({nums[i], nums[start], nums[end]});
+                    start++;
+                    end--;
+                    while (start < end && nums[start] == nums[start - 1]) {
+                        start++;
+                    }
+                    while (start < end && nums[end] == nums[end + 1]) {
+                        end--;
+                    }
+                } else if (value < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
         }
 
-        return finalTriplets;
+        return tripletsCollection;
     }
 };
