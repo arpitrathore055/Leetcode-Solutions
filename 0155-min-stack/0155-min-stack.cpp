e6e304272384dcbb0a77b5f65244997fa3989a88
Arpit Rathore <arpitrathore009@gmail.com>
2026-07-25 01:16:40 +0530

    Time: 61 ms (42.19%), Space: 150.9 MB (86.24%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,32 +1,26 @@
 class MinStack {
 
-    stack<int> mainStack;
-    stack<int> minStack;
+    stack<pair<int,int>> mainStack;
 
 public:
     MinStack() {
+        
     }
     
     void push(int val) {
-        mainStack.push(val);
-        int prevMin=(!minStack.empty())?minStack.top():INT_MAX;
-        if(prevMin == INT_MAX){
-            minStack.push(val);
-            return;
-        }
-        minStack.push(min(prevMin,val));
+        int prevMin=(mainStack.empty())?INT_MAX:mainStack.top().second;
+        mainStack.push({val,min(prevMin,val)});
     }
     
     void pop() {
         mainStack.pop();
-        minStack.pop();
     }
     
     int top() {
-        return mainStack.top();
+        return mainStack.top().first;
     }
     
     int getMin() {
-        return minStack.top();
+        return mainStack.top().second;
     }
 };
