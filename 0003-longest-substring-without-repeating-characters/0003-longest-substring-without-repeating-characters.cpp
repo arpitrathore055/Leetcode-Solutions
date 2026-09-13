9053e9a753831d11c59bf69b0e60a5e661aae494
Arpit Rathore <arpitrathore009@gmail.com>
2026-09-13 12:08:47 +0530

    Time: 42 ms (51.86%), Space: 19.5 MB (30.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,9 +1,44 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         int sLen=s.length();
+//         unordered_set<char> st;
+        
+//         int start=0;
+//         int end=0;
+//         int maxSubstringLen=0;
+        
+//         for(;end<sLen;){
+            
+//             if(!st.count(s[end])){
+//                 st.insert(s[end]);
+//                 ++end;
+//             }
+//             else{
+//                 maxSubstringLen=max(maxSubstringLen,end-start);
+//                 while(!st.count(s[end])){
+//                     st.erase(s[start]);
+//                     start++;
+//                 }
+                
+//             }
+            
+        
+//         }
+        
+//         return maxSubstringLen;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
         int sLen=s.length();
-        unordered_set<char> st;
+        unordered_map<char,int> lastSeen;
         
         int start=0;
         int end=0;
@@ -11,21 +46,17 @@ public:
         
         for(;end<sLen;){
             
-            if(!st.count(s[end])){
-                st.insert(s[end]);
-                ++end;
+            if(lastSeen.count(s[end])){
+                start=max(start,lastSeen[s[end]]+1);
             }
-            else{
-                maxSubstringLen=max(maxSubstringLen,end-start);
-                while(st.count(s[end]) && start<end){
-                    st.erase(s[start]);
-                    start++;
-                }
-                
-            }
-        
+            
+            lastSeen[s[end]]=end;
+            
+            maxSubstringLen=max(maxSubstringLen,end-start+1);
+            ++end;
+            
         }
-        maxSubstringLen=max(maxSubstringLen,end-start);
+        
         return maxSubstringLen;
         
     }
